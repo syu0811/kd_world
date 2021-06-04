e61f395142db765c6952ddb02803177ca712a473
syu0811 <codamayu494949@gmail.com>
2021-06-04 11:47:57 +0900

    追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "admin_user", email: "admin@example.com", password: "password", admin: true)
+User.create(name: "guest_user", email: "guest@example.com", password: "password", admin: false)
+User.create(name: "test_user", email: "test@example.com", password: "password", admin: false)
